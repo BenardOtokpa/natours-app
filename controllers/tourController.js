9f19af0d15f09d29315834200a8a5e42072cd845
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-09-27 10:15:40 +0100

    worked on the Aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -121,14 +121,36 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([
-      
-    ])
-
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+    res.status(200).json({
+      status: 'Success',
+      message: 'Stats calculated successfully',
+      data: {
+        stats,
+      },
+    });
   } catch (err) {
     res.status(400).json({
       status: 'Failed to delete tour',
       message: err,
     });
   }
-}
+};
