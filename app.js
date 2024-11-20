17c8fd7b11ca73cd7ecb2066d9784c7b5a518e52
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-11-20 14:04:52 +0100

    setting up proper security

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,20 @@ app.use(mongoSanitize());
 
 //Data sanitization against cross site scripting attatcks
 app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'difficulty',
+      'maxGroupSize',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'price',
+    ],
+  }),
+);
 
 // Serving static files from the public folder
 app.use(express.static(`${__dirname}/public`));
