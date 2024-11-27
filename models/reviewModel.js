aefe490cacd856d373cc0f8ffda9d04a5d1f4562
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-11-27 10:58:28 +0100

    Create reviewModel.js

---
+++ models/reviewModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      typeof: String,
+      required: [true, 'Review field is required'],
+      maxlength: [500, 'Review must be less than or equal to 500 characters'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'Rating field is required'],
+      min: 1,
+      max: 5,
+      validate: {
+        validator: Number.isInteger,
+        message: 'Rating must be an integer value between 1 and 5',
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a Tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a User'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
