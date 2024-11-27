35db55ca573cfae90e46069ec5e13b889adef779
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-11-27 13:15:42 +0100

    Update reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -40,6 +40,19 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+//Query Methods
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
