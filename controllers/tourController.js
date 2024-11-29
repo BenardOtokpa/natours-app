5433b9a7ada577f003954397b866759478b1cc15
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-11-29 09:46:48 +0100

    updated database with live data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('../models/tourModels');
+const Tour = require('../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 // const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-const Tour = require('./../../models/tourModels');
+const Tour = require('../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 // Connect to MongoDB
 const DB = process.env.DATABASE.replace(
@@ -15,11 +17,17 @@ mongoose.connect(DB).then((con) => console.log('Connected to MongoDB...'));
 
 // Read JSON file and parsing it
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf8'),
+);
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, {validateBeforeSave: false});
+    await Review.create(reviews);
     console.log('Data successfully uploaded to MongoDB...');
   } catch (err) {
     console.log(err);
@@ -31,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted from Database...');
   } catch (err) {
     console.log(err);
