0886cbe819fba4efa913a55e142cb8e3fa34789f
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2024-12-18 11:30:46 +0100

    working on updating user info

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const User = require('./../models/userModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // get tour data from collection
@@ -45,6 +46,21 @@ exports.getAccount = (req, res) => {
     title: 'Your Account',
   });
 };
-exports.updateUserData = (req, res) => {
-  console.log('updating', req.body);
-};
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  res.status(200).render('account', {
+    title: 'Your Account',
+    user: updatedUser,
+  });
+});
